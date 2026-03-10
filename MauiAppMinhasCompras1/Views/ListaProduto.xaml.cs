021f9037686721d761e50a98d9431be3be7a5fd1
Rafaela Pereira de Andrade <rp431040@gmail.com>
2026-03-10 19:31:51 -0300

    enviar para o código

--- MauiAppMinhasCompras1/Views/ListaProduto.xaml.cs
+++ MauiAppMinhasCompras1/Views/ListaProduto.xaml.cs
@@ -1,10 +1,15 @@
+using System.Collections.ObjectModel;
+
 namespace MauiAppMinhasCompras1.Views;
 
 public partial class ListaProduto : ContentPage
 {
+	ObservableCollection<Produto> lista = new ObservableCollection<Produto>();
 	public ListaProduto()
 	{
 		InitializeComponent();
+
+		lst_produtos.ItemsSource = lista;
 	}
 
     private void ToolbarItem_Clicked(object sender, EventArgs e)
